2a47730efc4b72588973aa5832bc0bcf25f89e66
jpelegrino1010 <julio.pelegrino@viseo.com>
2024-03-22 12:33:51 -0400

    first commit

---
+++ src/main/java/com/julioluis/devopcicd/DevopCiCdApplication.java
@@ -0,0 +1,13 @@
+package com.julioluis.devopcicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopCiCdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevopCiCdApplication.class, args);
+    }
+
+}
